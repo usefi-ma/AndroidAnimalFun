c538f6a8b1724b1cc17a6bb31a1f254af907b0fa
M-Mendoza <93346089+M-Mendoza@users.noreply.github.com>
2023-04-24 17:17:42 -0600

    Added 2 text feilds for kingdom and name and api call

--- app/src/main/java/com/example/animalfun/MainActivity.java
+++ app/src/main/java/com/example/animalfun/MainActivity.java
@@ -7,19 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ApiHandler.ApiCallback {
 
-Button start;
+    Button start;
+    TextView apiResponseAnimal;
+    TextView apiResponseKingdom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +26,52 @@ Button start;
 
 
         start=findViewById(R.id.startbtn);
+        apiResponseAnimal = findViewById(R.id.animalText);
+        apiResponseKingdom = findViewById(R.id.kingdomText);
 
-
-
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ApiHandler apiHandler = new ApiHandler();
+                apiHandler.makeApiCall("Cheetah", MainActivity.this);
+            }
+        });
     }
 
     public void loadMainMenu(View view) {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onSuccess(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    String name = jsonArray.getJSONObject(0).getString("name");
+                    JSONObject taxonomy = jsonArray.getJSONObject(0).getJSONObject("taxonomy");
+                    String kingdom = taxonomy.getString("kingdom");
+
+                    apiResponseAnimal.setText(name);
+                    apiResponseKingdom.setText(kingdom);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onError(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }

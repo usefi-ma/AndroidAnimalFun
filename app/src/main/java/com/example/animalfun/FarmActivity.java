7c590e1eaf3bf183ca9a2f14240d198eee18e14d
Kale White <kale_white@hotmail.com>
2023-04-25 01:08:52 -0600

    added textviews and calls for farm animals

--- app/src/main/java/com/example/animalfun/FarmActivity.java
+++ app/src/main/java/com/example/animalfun/FarmActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,16 +31,49 @@ import java.util.HashMap;
 
 public class FarmActivity extends AppCompatActivity  {
 
-    TextView farmTest;
-
-    Button cow;
+    ImageView cow;
+    ImageView chicken;
+    ImageView pig;
+    ImageView horse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm);
+        horse = findViewById(R.id.horse);
+        cow = findViewById(R.id.cow);
+        pig = findViewById(R.id.pig);
+        chicken = findViewById(R.id.chicken);
 
 
+        View.OnClickListener onClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                String imageName = getResources().getResourceEntryName(v.getId());
+                Log.d("FarmActivity", "imageName: "+ imageName);
+                ApiHandler apiHandler = new ApiHandler();
+                apiHandler.makeApiCall(imageName, new ApiHandler.ApiCallback() {
+                    @Override
+                    public void onSuccess(String response) {
+                        Intent intent = new Intent(FarmActivity.this, FarmInfo.class);
+                        intent.putExtra("animalName", imageName);
+//                        intent.putExtra("animalInfo", response);
+                        Log.d("Intent", "putextra:" + intent);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(FarmActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+        cow.setOnClickListener(onClickListener);
+        horse.setOnClickListener(onClickListener);
+        pig.setOnClickListener(onClickListener);
+        chicken.setOnClickListener(onClickListener);
     }
 }
 
--- app/src/main/java/com/example/animalfun/FarmInfo.java
+++ app/src/main/java/com/example/animalfun/FarmInfo.java
@@ -3,17 +3,85 @@ package com.example.animalfun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FarmInfo extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    Button testButton;
+public class FarmInfo extends AppCompatActivity implements ApiHandler.ApiCallback {
+
+    TextView animalName;
+    TextView kingdomName;
+    TextView slogan;
+    TextView prey;
+    TextView diet;
+
+    TextView df;
+    TextView habitat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_info);
 
+        animalName = findViewById(R.id.animalNameFarm);
+        kingdomName = findViewById(R.id.kingdomNameFarm);
+        slogan = findViewById(R.id.farmSlogan);
+        prey = findViewById(R.id.farmPrey);
+        diet = findViewById(R.id.farmDiet);
+        df = findViewById(R.id.farmDf);
+        habitat = findViewById(R.id.farmHabitat);
 
+
+
+        String name = getIntent().getStringExtra("animalName");
+        Log.d("FarmInfo", "Name: " + name);
+        ApiHandler apiHandler = new ApiHandler();
+        apiHandler.makeApiCall(name, FarmInfo.this);
+    }
+
+    @Override
+    public void onSuccess(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    String Name = jsonArray.getJSONObject(0).getString("name");
+                    JSONObject characteristics = jsonArray.getJSONObject(0).getJSONObject("characteristics");
+                    String Slogan = characteristics.getString("slogan");
+                    String Prey = characteristics.getString("prey");
+                    String Diet = characteristics.getString("diet");
+                    String Distinctfeature = characteristics.getString("distinctive_feature");
+                    String Habitat = characteristics.getString("habitat");
+
+                    animalName.setText(Name);
+                    slogan.setText(Slogan);
+                    prey.setText(Prey);
+                    diet.setText(Diet);
+                    df.setText(Distinctfeature);
+                    habitat.setText(Habitat);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onError(String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(FarmInfo.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
